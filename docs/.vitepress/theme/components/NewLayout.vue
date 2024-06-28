de3a2174f6b5ea28ba4565e273f28f8bce968eef
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-06-29 00:43:44 +0800

    fix: Giscus refresh when change page

--- docs/.vitepress/theme/components/NewLayout.vue
+++ docs/.vitepress/theme/components/NewLayout.vue
@@ -24,14 +24,16 @@
             </ClientOnly>
         </template>
         <template #doc-after>
-            <Giscus id="comments" repo="dreamhunter2333/dreamhunter-blog" repoId="MDEwOlJlcG9zaXRvcnkzMzIyNTA3MTc="
-                category="General" categoryId="DIC_kwDOE82-Xc4CgbBf" mapping="pathname" reactionsEnabled="1"
-                emitMetadata="0" inputPosition="top" :theme="isDark ? 'dark' : 'light'" lang="zh-CN" loading="lazy" />
+            <div :key="giscusKey">
+                <Giscus id="comments" repo="dreamhunter2333/dreamhunter-blog" repoId="MDEwOlJlcG9zaXRvcnkzMzIyNTA3MTc="
+                    category="General" categoryId="DIC_kwDOE82-Xc4CgbBf" mapping="pathname" reactionsEnabled="1"
+                    emitMetadata="0" inputPosition="top" :theme="isDark ? 'dark' : 'light'" lang="zh-CN"
+                    loading="lazy" />
+            </div>
         </template>
     </Layout>
 </template>
 <script setup>
-
 import DefaultTheme from 'vitepress/theme'
 import { useData, withBase } from "vitepress";
 import Giscus from '@giscus/vue';
@@ -39,7 +41,10 @@ import { computed } from 'vue';
 import { NConfigProvider, darkTheme, NTag, NSpace, NIcon } from "naive-ui";
 import { Clock, Tag } from '@vicons/fa';
 
-const { isDark } = useData()
-const { Layout } = DefaultTheme
-const naiveTheme = computed(() => isDark.value ? darkTheme : null)
+const { isDark, frontmatter } = useData();
+const { Layout } = DefaultTheme;
+const naiveTheme = computed(() => isDark.value ? darkTheme : null);
+const giscusKey = computed(() => {
+    return `${frontmatter.value?.title}-${frontmatter.value?.date}-${frontmatter.value?.tags.join('-')}`
+})
 </script>
