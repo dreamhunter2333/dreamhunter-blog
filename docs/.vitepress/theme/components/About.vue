b24bdb1d4f00823e58003f69cdca3e127a11ebd0
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-07-20 01:03:34 +0800

    fix: css VPTeamMembers min width

--- docs/.vitepress/theme/components/About.vue
+++ docs/.vitepress/theme/components/About.vue
@@ -1,16 +1,17 @@
 <script lang="ts" setup>
-import { VPTeamMembers } from 'vitepress/theme'
+// @ts-ignore
+import VPTeamMembersItem from 'vitepress/dist/client/theme-default/components/VPTeamMembersItem.vue';
 import { useData } from 'vitepress'
 import { computed } from 'vue'
 import { CustomThemeConfig } from '../type';
 const { theme } = useData<CustomThemeConfig>();
-const members = computed(() => [theme.value.about])
+const member = computed(() => theme.value.about)
 </script>
 
 <template>
     <ClientOnly>
-        <div v-if="members" style="display: flex; justify-content: center; width: 100%; ">
-            <VPTeamMembers :members="members" />
+        <div style="justify-content: center; width: 100%; ">
+            <VPTeamMembersItem :member="member" />
         </div>
     </ClientOnly>
 </template>
